993016367d0827ade07d8c649bfeda68e3a0d5be
Go <loak155.0620@gmail.com>
2022-05-25 17:03:03 +0900

    Create excel2pdf.ps1

---
+++ excel2pdf.ps1
@@ -0,0 +1,34 @@
+if ( $args -eq $null ) {
+    Write-Error '引数がありません'
+}
+
+if (Test-Path $args[0]) {
+    $excel = New-Object -ComObject Excel.Application
+    $excel.Visible = $false
+
+    # 拡張子変更
+    $newpdf = $args[0] -replace '\.xlsx$', '.pdf'
+
+
+    try {
+        $book = $excel.Workbooks.Open($args[0])
+
+        # PDF
+        $book.ExportAsFixedFormat([Microsoft.Office.Interop.Excel.XlFixedFormatType]::xlTypePDF, $newpdf)
+
+        $book.Close($false)
+
+    }
+    catch {
+        Write-Error 'エラーが発生しました'
+    }
+    finally {
+        $excel.Quit()
+        $excel = $null
+        [GC]::Collect()
+    }
+
+}
+else {
+    Write-Error 'ファイルが見つかりません'
+}
